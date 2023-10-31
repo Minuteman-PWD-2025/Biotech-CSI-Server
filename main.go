16acd94f3e7936ca91605664e49233619d9d3705
KitChaos <cmeow176@gmail.com>
2023-10-31 08:29:02 -0400

    Made reading Json

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+type Data struct {
+	Name         string
+	IsCheckedOut bool
+	CheckedOutBy string
+	TimeOut      string
+}
 
 func main() {
 	fmt.Printf("Starting Server...\n")
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/Hello", GetHello)
+
 	http.ListenAndServe(":1234", nil)
 
 }
@@ -19,11 +28,19 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 
 	case "GET":
+		if r.FormValue("id") != "" {
+			content, err := ioutil.ReadFile("./Data/" + string(r.FormValue("id")) + ".json")
+			fmt.Printf(r.FormValue("id") + "\n")
+			if err != nil {
+				log.Fatal("Error when opening file: ", err)
+			}
+			var payload Data
+			err = json.Unmarshal(content, &payload)
+			if err != nil {
+				log.Fatal("Error during Unmarshal(): ", err)
+			}
+			fmt.Printf(payload.Name + "\n")
+		}
 
 	}
-}
-func GetHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hello World")
-	io.WriteString(w, "Hello\n")
-
 }
