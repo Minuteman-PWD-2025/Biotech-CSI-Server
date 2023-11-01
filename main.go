455585eb36dfbacc5a76328c0234c237213aff0b
TimidBagel <slaprabbit805@gmail.com>
2023-11-01 11:14:17 -0400

    Updated comments so they are more betterer

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-//this is in need of error handling in some places
 import (
 	"encoding/json"
 	"errors"
@@ -12,150 +11,153 @@ import (
 )
 
 func main() {
-	//Currently can only get the server working on 8080
+	// Start the HTTP server on port 8080
 	fmt.Printf("Starting Server...\n")
 	http.HandleFunc("/", getRoot)
-
 	http.ListenAndServe(":8080", nil)
-
 }
-func getRoot(w http.ResponseWriter, r *http.Request) {
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
+		fmt.Println("Received a POST request")
 
-		//by writing before deleting, it automatically handles the possible issue of the file to delete not existing, so that is nice
-		fmt.Println("Got A post")
-
+		// Read the request body
 		reqBody, err := io.ReadAll(r.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
 		data := ""
 		for i := 0; i < int(r.ContentLength); i++ {
-
 			data += string(int(reqBody[i]))
 		}
+
+		// Check if it's an edit request and if the file already exists
 		isEdit := r.FormValue("Edit")
 		doesAlreadyExist := AffirmExistanceOfFile(r)
 		fmt.Println(doesAlreadyExist)
+
 		if isEdit == "true" {
-			if doesAlreadyExist { //Means you are editing a file that exists so everything is ok
-				//On the frontend we will need to grab the data of the file, and then fill it in, such that they only need to edit things
-
+			if doesAlreadyExist {
+				// If editing an existing file, write the new data to it
 				os.WriteFile("./Data/"+r.FormValue("id")+".json", []byte(data), 0644)
-
-			} else { //Uh oh, the file does not exist, will ask if you want to create it
+			} else {
+				// If the file does not exist, create a new file
 				fmt.Println("This file does not exist yet")
 				os.WriteFile("./Data/"+r.FormValue("id")+".json", []byte(data), 0644)
 			}
 		} else {
-			if doesAlreadyExist { //uh oh, you are trying to creat a new file that already exists
-				//Replace with some proc for confirmation
-
+			if doesAlreadyExist {
+				// Editing a file that already exists
 				os.WriteFile("./Data/"+r.FormValue("id")+".json", []byte(data), 0644)
-
-			} else { //You are creating a new file that does not exist yet, so go ahead with it
-
+			} else {
+				// Creating a new file
 				os.WriteFile("./Data/"+r.FormValue("id")+".json", []byte(data), 0644)
 			}
 		}
 
-		//For removing from the dat
+		// For removing a file
 		if r.FormValue("Deletion") == "true" {
+			// Remove the file if requested
 			os.Remove("./Data/" + r.FormValue("id") + ".json")
 		}
+
 	case "GET":
 		if r.FormValue("id") != "" {
-			//Read Json from the file requested, based on id
-
-			//This line isnt needed and will be removed in my next push
-			//io.WriteString(w, payload.Name)
-			//f, err := os.Open()
+			// Serve the JSON file based on the 'id' query parameter
 			http.ServeFile(w, r, "./Data/"+string(r.FormValue("id"))+".json")
 		}
 		if r.FormValue("WhichKey") != "" {
-			//Any special keywords will be in all caps and enclosed in pipes
 			if r.FormValue("TargetKey") == "|ALL|" {
-
+				// Handle special keyword
 			}
 			TargetCultures := GetAllRowsWithVal(r)
 			os.WriteFile("QuickData", []byte(TargetCultures), 0644)
 			http.ServeFile(w, r, "QuickData")
 			os.Remove("QuickData")
 		}
-		//Get all the items with a matching
-
 	}
 }
+
 func AffirmExistanceOfFile(r *http.Request) bool {
+	// Check if a file exists
 	if _, err := os.Stat("./Data/" + r.FormValue("id") + ".json"); err == nil {
 		return true
-
 	} else if errors.Is(err, os.ErrNotExist) {
 		return false
-
 	} else {
-		panic("What are you doing here how did you Schrodinger")
-
+		panic("Unexpected error occurred")
 	}
 }
-func GetAllRowsWithVal(r *http.Request) string { //Just return it all for a string right now
+
+func GetAllRowsWithVal(r *http.Request) string {
+	// Check if the "TargetKey" query parameter is provided in the request
 	if r.FormValue("TargetKey") != "" {
-		alls := ""
+		alls := "" // Initialize an empty string to store the results
+
+		// Check if the "TargetKey" is not "|ALL|"
 		if r.FormValue("TargetKey") != "|ALL|" {
+			// Read the list of files in the 'Data' directory
 			dir, err := os.ReadDir("./Data")
 			if err != nil {
 				panic(err)
 			}
 
+			// Iterate through the files in the directory
 			for _, e := range dir {
-
+				// Read the content of each file
 				content, err := os.ReadFile("./Data/" + string(e.Name()))
-
 				if err != nil {
 					log.Fatal("Error when opening file: ", err)
 				}
 
+				// Parse the JSON content into a map
 				var payload map[string]string
 				err = json.Unmarshal(content, &payload)
 				if err != nil {
 					log.Fatal("Error during Unmarshal(): ", err)
 				}
+
+				// Check if the value of "WhichKey" matches the "TargetKey"
 				if payload[r.FormValue("WhichKey")] == r.FormValue("TargetKey") {
+					// Append the "name" value from the JSON data to the result string
 					alls += payload["name"] + "\n"
 				}
-
 			}
 		} else {
+			// If "TargetKey" is "|ALL|", retrieve all values of "WhichKey"
 			dir, err := os.ReadDir("./Data")
 			if err != nil {
 				panic(err)
 			}
 
+			// Iterate through the files in the directory
 			for _, e := range dir {
-
+				// Read the content of each file
 				content, err := os.ReadFile("./Data/" + string(e.Name()))
-
 				if err != nil {
 					log.Fatal("Error when opening file: ", err)
 				}
 
+				// Parse the JSON content into a map
 				var payload map[string]string
 				err = json.Unmarshal(content, &payload)
 				if err != nil {
 					log.Fatal("Error during Unmarshal(): ", err)
 				}
 
+				// Append the value of "WhichKey" from the JSON data to the result string
 				alls += payload[r.FormValue("WhichKey")] + "\n"
-
 			}
 		}
+
+		// Check if any matches were found, if not, return a message
 		if alls == "" {
 			alls = "No Matches Found\n"
 		}
 		return alls
-
 	}
-	return "How did you get here"
+
+	// Return an "Invalid request" message if "TargetKey" is not provided in the request
+	return "Invalid request"
 }
