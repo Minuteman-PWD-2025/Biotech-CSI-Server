c61edb43b44f35ce62485a06cf68ac8fb88b7094
Drew <83474944+drewuw@users.noreply.github.com>
2023-11-02 13:44:22 -0400

    mas

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
-    "fmt"
-    _ "github.com/lib/pq"  // PostgreSQL driver for go
+	"fmt"
+	"html/template"
+	_ "github.com/lib/pq"
 )
 
+type User struct {
+	ID int
+	Name  string
+	Email string
+	Password string
+	Privilege int
+}
+// for debugging the sending and recieving
+var tmpl = template.Must(template.New("index").Parse(`
+<html>
+<head>
+    <title>User List</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+			<th>Password</th>
+			<th>Privilege</th>
+
+        </tr>
+        {{range .}}
+            <tr>
+                <td>{{.ID}}</td>
+                <td>{{.Name}}</td>
+                <td>{{.Email}}</td>
+				<td>.Password</td>
+				<td>.Privilege</td>
+            </tr>
+        {{end}}
+    </table>
+</body>
+</html>
+`))
 
 func main() {
 	fmt.Printf("Starting Server...\n")
