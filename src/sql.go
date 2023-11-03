f5c0e90de8d3d60046a740400ee9616b7c086190
Ben Champagne <donkeykong797@gmail.com>
2023-11-03 09:51:01 -0400

    cant seem to get a return value

--- src/sql.go
+++ src/sql.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	fmt "fmt"
-	os "os"
 
 	"database/sql"
 
 	_ "github.com/lib/pq"
 )
 
-var db_name = os.Getenv("db_name")
-var db_user = os.Getenv("db_user")
-var db_pass = os.Getenv("db_pass")
+const (
+	db_name = "postgres"
+	db_user = "postgres"
+	db_pass = "admin"
+	db_host = "127.0.0.1"
+	db_port = "5432"
+)
 
 func testConnection() error {
 	fmt.Printf("Testing Connection to Database...\n")
-	connStr := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", db_user, db_name, db_pass)
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", db_host, db_port, db_user, db_pass, db_name)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return err
 	}
 
+	defer db.Close()
+
 	var rows []string
 
-	query, err := db.Query("SELECT name FROM people;")
+	query, err := db.Query("SELECT * FROM people;")
 
 	if err != nil {
 		panic(err)
@@ -33,6 +38,5 @@ func testConnection() error {
 
 	fmt.Printf("Found rows: %s \n", rows)
 
-	defer db.Close()
 	return err
 }
