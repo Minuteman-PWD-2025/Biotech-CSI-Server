2244c9d9e8c6b0ad0d82a62281f9fd964978e06a
KitChaos <cmeow176@gmail.com>
2023-11-14 13:25:54 -0500

    Fandangled with the issue, added some dummy data for serving

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"net/http"
 	"strings"
 	"time"
@@ -125,14 +126,58 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		if r.FormValue("token") != "" {
 			if r.FormValue("table") != "" {
 				rows := GetTable(r.FormValue("table"))
+				// names := make([]string, 0)
+				// cols, _ := rows.Columns()
+				// leng := len(cols)
+				// for rows.Next() {
+				// 	quick:= make([]string, leng)
+
+				// 	if err := rows.Scan(&quick); err != nil {
+				// 		panic(err)
+				// 	}
+				// 	names = append(names, name)
+				// }
+				// // Check for errors from iterating over rows.
+				// if err := rows.Err(); err != nil {
+				// 	panic(err)
+				// }
+				// log("%s are %d years old", strings.Join(names, ", "), "4")
 				//#region Placeholder
 				var user_id string
 				var name string
+				var FinString string
+
 				for rows.Next() {
 					rows.Scan(&user_id, &name)
 					fmt.Printf("ID: %s\nName: %s\n\n", user_id, name)
+					FinString += "ID: " + user_id + "\nName: " + name + "\n"
 				}
+				//os.WriteFile("data", []byte(FinString), 0644)
+				http.ServeFile(w, r, "data.json")
+				//fmt.Print(rows)
+				//cols, _ := rows.Columns()
+
+				// leng := len(cols)
+				// //#region Placeholder
+				// var Datas [2000]string
+				// var name string
+				// var FinString string
+				// //var ph string
+				// i := 0
+				// for rows.Next() {
+				// 	for j := 0; j < leng; j++ {
+				// 		rows.Scan(&Datas[i+j], &name)
+				// 		//fmt.Printf("ID: %s\nName: %s\n\n", user_id, name)
+				// 		FinString += Datas[i+j] + "\n"
+
+				// 	}
+				// 	i++
+
+				// }
+				// fmt.Print(FinString)
+
 				//#endregion
+				//rows := GetTable(r.FormValue("table"))     //#region Placeholder     //cols, err := rows.Columns()     //if err != nil {     // panic(err)     //}     //leng := len(cols)     var Datas string //I hate this, there must be a better way     i := 0     var FinString string     for rows.Next() {      //for j := 0; j < leng; j++ {      rows.Scan(&Datas)      FinString += Datas + "\n"      //}       //FinString += Datas[i] + "\n"      i++     }     fmt.Print(FinString)      //#endregion
 			}
 		} else {
 			if r.FormValue("email") != "" && r.FormValue("password") != "" {
